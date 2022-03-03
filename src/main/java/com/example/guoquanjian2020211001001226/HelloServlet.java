1ee780fd904a8afd8ff6377fca730e5b2574dbfd
Bertram <228343760@qq.com>
2022-03-03 16:25:11 +0800

    second commit

--- src/main/java/com/example/guoquanjian2020211001001226/HelloServlet.java
+++ src/main/java/com/example/guoquanjian2020211001001226/HelloServlet.java
@@ -19,6 +19,7 @@ public class HelloServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
+        out.println("This is my JSP homework!");
         out.println("</body></html>");
     }
 
